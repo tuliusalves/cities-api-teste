5c441cd0fbc7db226bfcdd256984b4a8a973cdd1
tulius <tulius_sr@outlook.com>
2021-11-02 11:06:39 -0300

    Primeiro commit
    Projeto de cálculo de distância entre locais.

---
+++ src/main/java/com/github/tuliusalves/citiesapi/states/StateRepository.java
@@ -0,0 +1,7 @@
+package com.github.tuliusalves.citiesapi.states;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State,Long> {
+
+}
